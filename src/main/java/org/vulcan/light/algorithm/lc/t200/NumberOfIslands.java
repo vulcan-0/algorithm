97754da70ac5370c3884432094fcbbba620f0885
Sam Lu <luxiaocong524@163.com>
2022-06-17 12:56:31 +0800

    feat: 200. 岛屿数量 + 234. 回文链表

---
+++ src/main/java/org/vulcan/light/algorithm/lc/t200/NumberOfIslands.java
@@ -0,0 +1,65 @@
+package org.vulcan.light.algorithm.lc.t200;
+
+/**
+ * 200. 岛屿数量
+ * https://leetcode.cn/problems/number-of-islands/
+ *
+ * @author Sam Lu
+ * @date 2022/06/17
+ */
+public class NumberOfIslands {
+
+    /**
+     * // 输入：grid = [
+     * //   ["1","1","1","1","0"],
+     * //   ["1","1","0","1","0"],
+     * //   ["1","1","0","0","0"],
+     * //   ["0","0","0","0","0"]
+     * // ]
+     * // 输出：1
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}};
+
+        NumberOfIslands numberOfIslands = new NumberOfIslands();
+        System.out.println(numberOfIslands.numIslands(grid));
+    }
+
+    /**
+     * 深度遍历，所到之处置为0
+     *
+     * @param grid
+     * @return
+     */
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int row = 0; row < grid.length; row++) {
+            for (int column = 0; column < grid[0].length; column++) {
+                if (grid[row][column] == '1') {
+                    dfs(grid, row, column);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int row, int column) {
+        if (row < 0 || column < 0 || row >= grid.length || column >= grid[0].length
+                || grid[row][column] == '0') {
+            return;
+        }
+        grid[row][column] = '0';
+        dfs(grid, row - 1, column);
+        dfs(grid, row + 1, column);
+        dfs(grid, row, column - 1);
+        dfs(grid, row, column + 1);
+    }
+
+}
---
+++ src/main/java/org/vulcan/light/algorithm/lc/t200/PalindromeLinkedList.java
@@ -0,0 +1,66 @@
+package org.vulcan.light.algorithm.lc.t200;
+
+import org.vulcan.light.algorithm.zcommon.ListNode;
+
+/**
+ * 234. 回文链表
+ * https://leetcode.cn/problems/palindrome-linked-list/
+ *
+ * @author Sam Lu
+ * @date 2022/06/17
+ */
+public class PalindromeLinkedList {
+
+    /**
+     * 输入：head = [1,2,2,1]
+     * 输出：true
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1)
+                .setNext(new ListNode(2)
+                        .setNext(new ListNode(2)
+                                .setNext(new ListNode(1))));
+
+        PalindromeLinkedList palindromeLinkedList = new PalindromeLinkedList();
+        System.out.println(palindromeLinkedList.isPalindrome(head));
+    }
+
+    /**
+     * 快慢指针
+     *
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        boolean result = true;
+        while (result && slow != null) {
+            if (head.val != slow.val) {
+                result = false;
+            }
+            head = head.next;
+            slow = slow.next;
+        }
+        return result;
+    }
+
+    private ListNode reverse(ListNode node) {
+        ListNode prev = null;
+        ListNode curr = node;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+}
